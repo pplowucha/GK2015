d7d6de4567dd24ce741c2dd8163e1e0c8a542608
pplowucha <pplowucha@gmail.com>
2016-03-06 20:25:55 +0100

    Added files via upload

---
+++ zajecia 2/program1.cpp
@@ -0,0 +1,182 @@
+﻿// czternastokat
+
+#include "stdafx.h"
+#ifdef __APPLE__
+#include <GLUT/glut.h>
+#else
+#include <GL/glut.h>
+#endif
+
+#include "math.h"
+
+// Definiujemy stałą o wartości liczby PI
+#define GL_PI 3.1415f
+// Wielkość obrotów
+static GLfloat xRot = 0.0f;
+static GLfloat yRot = 0.0f;
+// Wywoływana w celu przerysowania sceny
+void RenderScene(void)
+{
+	GLfloat x, y, z, angle; // Zmienne przechowujące współrzędne i kąty
+	GLfloat sizes[2]; // Do zapisania dopuszczalnego zakresu wielkości
+	GLfloat step; // Zapamiętuje obsługiwany przyrost rozmiaru
+	GLfloat curSize; // Zapamęituje aktualny rozmiar
+	// Wyczyszczenie okna aktualnym kolorem czyszczącym
+	glClear(GL_COLOR_BUFFER_BIT);
+	// Zapisanie stanu macierzy i wykonanie obrotu
+	glPushMatrix();
+	glRotatef(xRot, 0.0f, 0.0f, 0.0f);
+	glRotatef(yRot, 0.0f, 1.0f, 0.0f);
+	// Pobranie dopuszczalnych rozmiarów punktów
+	glGetFloatv(GL_POINT_SIZE_RANGE, sizes);
+	glGetFloatv(GL_POINT_SIZE_GRANULARITY, &step);
+	// Ustalenie początkowego rozmiaru punktu
+	curSize = sizes[0];
+	// Ustalenie pocztkowej współrzędnej z
+	z = -50.0f;
+	// Obracamy się w kółko trzy razy
+	for (angle = 0.0f; angle <= (2.0f*3.1415f)*14.0f; angle += 0.2f)
+	{
+		// Wyliczenie wartości x i y na kole
+		x = 50.0f*sin(angle);
+		y = 50.0f*cos(angle);
+		// Zdefiniowanie rozmiaru punktu przed utworzeniem obiektu podstawowego
+		glPointSize(curSize);
+		// Rysowanie punktu
+		glBegin(GL_POINTS);
+		glVertex3f(x, y, z);
+		glEnd();
+		// Powiększenie współrzędnej z i rozmiaru punktu
+		z += 0.4f;
+		curSize += step/2;
+	}
+	// Odtworzenie macierzy przekształceń
+	glPopMatrix();
+	// Wykonanie poleceń rysowania
+	glutSwapBuffers();
+}
+// Ta funkcja wykonuje wszystkie konieczne inicjalizacje kontekstu renderowania
+void SetupRC()
+{
+	// Czarne tło
+	glClearColor(0.0f, 0.0f, 0.0f, 1.0f);
+	// Będziemy rysować kolorem zielonym
+	glColor3f(0.0f, 1.0f, 0.0f);
+}
+void SpecialKeys(int key, int x, int y)
+{
+	if (key == GLUT_KEY_UP)
+		xRot -= 5.0f;
+	if (key == GLUT_KEY_DOWN)
+		xRot += 5.0f;
+	if (key == GLUT_KEY_LEFT)
+		yRot -= 5.0f;
+	if (key == GLUT_KEY_RIGHT)
+		yRot += 5.0f;
+	if (key > 356.0f)
+		xRot = 0.0f;
+	if (key < -1.0f)
+		xRot = 355.0f;
+	if (key > 356.0f)
+		yRot = 0.0f;
+	if (key < -1.0f)
+		yRot = 355.0f;
+	// Odświeżenie zawartości okna
+	glutPostRedisplay();
+}
+void ChangeSize(int w, int h)
+{
+	GLfloat nRange = 100.0f;
+	// Zabezpieczenie przed dzieleniem przez zero
+	if (h == 0)
+		h = 1;
+	// Ustalenie wymiarów widoku na zgodnych z wymiarami okna
+	glViewport(0, 0, w, h);
+	// Ponowne ustawienie stosu macierzy rzutowania
+	glMatrixMode(GL_PROJECTION);
+	glLoadIdentity();
+	// Utworzenie przestrzeni ograniczającej (lewo, prawo, dół, góra, blisko, daleko)
+	if (w <= h)
+		glOrtho(-nRange, nRange, -nRange*h / w, nRange*h / w, -nRange, nRange);
+	else
+		glOrtho(-nRange*w / h, nRange*w / h, -nRange, nRange, -nRange, nRange);
+	// Ponowne ustawienie stosu macierzy rzutowania
+	glMatrixMode(GL_MODELVIEW);
+	glLoadIdentity();
+}
+
+void Menu(int value)
+{
+	switch (value)
+	{
+		//
+
+	case 'red':
+		glColor3f(1.0f, 0.0f, 0.0f);
+		break;
+
+	case 'gr':
+		glColor3f(0.0f, 1.0f, 0.0f);
+		
+		break;
+
+		//
+	case 'blue':
+		
+		glColor3f(0.0f,0.0f, 1.0f);
+		break;
+
+	case 'wh':
+		glColor3f(1.0f, 1.0f, 1.0f);
+
+		break;
+
+	case 'br':
+		glColor3f(0.6f, 0.4f, 0.12f);
+
+		break;
+
+	
+
+
+
+		// wyjęcie
+	case 'exit':
+		exit(0);
+	}
+}
+
+int main(int argc, char* argv[])
+{
+	glutInit(&argc, argv);
+	glutInitDisplayMode(GLUT_DOUBLE | GLUT_RGB | GLUT_DEPTH);
+	glutInitWindowSize(1024, 800);
+	glutCreateWindow("Points Size Example");
+
+
+
+
+	glutReshapeFunc(ChangeSize);
+	glutSpecialFunc(SpecialKeys);
+	glutDisplayFunc(RenderScene);
+
+
+
+
+	SetupRC();
+
+	// utworzenie podmenu - Prymitywy
+	int MenuPrimitive = glutCreateMenu(Menu);
+	glutAddMenuEntry("Kolor czerwony.", 'red');
+	glutAddMenuEntry("Kolor zielony.", 'gr');
+	glutAddMenuEntry("Kolor niebieski.", 'blue');
+	glutAddMenuEntry("Kolor bialy.", 'wh');
+	glutAddMenuEntry("Kolor brazowy.", 'br');
+	glutAddMenuEntry("Wyjscie", 'exit');
+
+	// określenie przycisku myszki obsługującego menu podręczne
+	glutAttachMenu(GLUT_RIGHT_BUTTON);
+
+	glutMainLoop();
+	return 0;
+}
---
+++ zajecia 2/program3.cpp
@@ -0,0 +1,292 @@
+﻿// OpenGL_TRIANGLE_ConsoleApplication.cpp : Defines the entry point for the console application.
+// czternastokat
+
+#include "stdafx.h"
+#include "math.h"
+
+#include "stdafx.h"
+#ifdef __APPLE__
+#include <GLUT/glut.h>
+#else
+#include <GL/glut.h>
+#endif
+
+#define GL_PI 3.1415f
+
+enum
+{
+	EXIT                            // wyjęcie
+};
+
+// Wielkość obrotów
+static GLfloat xRot = 0.0f;
+static GLfloat yRot = 0.0f;
+
+static GLboolean iCull, iDepth, iOutline, iSmooth;
+
+// Funkcja wykonuje wszystkie konieczne inicjalizacje kontekstu renderowania
+void SetupRC()
+{
+	GLuint texture;
+	int x, y;
+	char pixels[256 * 256];
+	GLint iWidth = 256;
+	GLint iHeight = 256;
+	GLint iComponents = GL_RGB;
+	GLenum eFormat = {
+		GL_RGBA
+	};
+	// Wartoci i współrzdne światła
+	GLfloat whiteLight[] = { 0.05f, 0.05f, 0.05f, 1.0f };
+	GLfloat sourceLight[] = { 0.25f, 0.25f, 0.25f, 1.0f };
+	GLfloat lightPos[] = { -10.f, 5.0f, 5.0f, 1.0f };
+	glEnable(GL_DEPTH_TEST); // Usuwanie ukrytych powierzchni
+	glEnable(GL_CULL_FACE); // Nie bdziemy prowadzi oblicze wntrza samolotu
+	glFrontFace(GL_CCW); // Wielokty z nawiniciem przeciwnym do ruchu wskazówek zegara
+	// Włczenie owietlenia
+	glEnable(GL_LIGHTING);
+	// Konfiguracja i włączenie światła numer 0
+	glLightModelfv(GL_LIGHT_MODEL_AMBIENT, whiteLight);
+	glLightfv(GL_LIGHT0, GL_AMBIENT, sourceLight);
+	glLightfv(GL_LIGHT0, GL_DIFFUSE, sourceLight);
+	glLightfv(GL_LIGHT0, GL_POSITION, lightPos);
+	glEnable(GL_LIGHT0);
+	// Włczenie ledzenia kolorów
+	glEnable(GL_COLOR_MATERIAL);
+	// Właciwoci owietlenia otoczenia i rozproszenia
+	// bd ledzi wartoci podawane funkcji glColor
+	glColorMaterial(GL_FRONT, GL_AMBIENT_AND_DIFFUSE);
+	// Czarne tło
+	glClearColor(1.0f, 0.5f, 0.5f, 1.0f);
+	// Ładowanie tekstury
+	glPixelStorei(GL_UNPACK_ALIGNMENT, 1);
+
+	// Generate a name for the texture
+	glGenTextures(1, &texture);
+
+	glBindTexture(GL_TEXTURE_2D, texture);
+
+
+	for (y = 0; y < 256; y++)
+	{
+		for (x = 0; x < 256; x++)
+			pixels[y * 256 + x] = rand() % 256;
+	}
+
+	glTexImage2D(GL_TEXTURE_2D, 0, GL_LUMINANCE, 256, 256, 50, GL_LUMINANCE, GL_UNSIGNED_BYTE, pixels);
+	glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_LINEAR);
+	glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_LINEAR);
+	glTexEnvi(GL_TEXTURE_ENV, GL_TEXTURE_ENV_MODE, GL_MODULATE);
+	glEnable(GL_TEXTURE_2D);
+
+	//// Czarne tło
+	//glClearColor(0.0f, 0.0f, 0.0f, 1.0f);
+	//// Kolor rysujący - zielony
+	//glColor3f(0.0f, 1.0f, 0.0f);
+	//// Model cieniowania kolorów - płaski
+	//glShadeModel(GL_FLAT);
+	//// Wielokąty o nawinięciu zgodnym z ruchem wskazówek zegara traktowane są
+	//// jako skierowane do przodu. Takie ustawienie jest konieczne, ponieważ
+	//// korzystamy z wachlarzy trójkątów.
+	//glFrontFace(GL_CW);
+}
+// Wywoływana w celu przerysowania sceny
+void RenderScene(void)
+{
+	GLfloat x, y, angle; // Przechowują wartości współrzędnych i kąta
+	int iPivot = 1; // Do oznaczania zamiennych kolorów
+	// Wyczyszczenie okna i bufora głębi
+	glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
+	// Włączenie lub wyłączenie mechanizmu eliminowania ukrytych powierzchni
+
+
+	if (iSmooth)
+		glShadeModel(GL_FLAT);
+	else
+		glShadeModel(GL_SMOOTH);
+
+	if (iCull)
+		glEnable(GL_CULL_FACE);
+	else
+		glDisable(GL_CULL_FACE);
+	// Włączenie lub wyłączenie mechanizmu sprawdzania głębi
+	/*if (iDepth)
+		glEnable(GL_DEPTH_TEST);
+	else
+		glDisable(GL_DEPTH_TEST);*/
+	// Jeżeli ten znacznik będzie ustawiony, to wielokąty zwrócone
+	// tyłem do widza będą rysowane tylko w postaci szkieletu
+	if (iOutline)
+		glPolygonMode(GL_BACK, GL_LINE);
+	else
+		glPolygonMode(GL_BACK, GL_FILL);
+	// Zapisanie stanu macierzy i wykonanie obrotu
+	glPushMatrix();
+	glRotatef(xRot, 1.0f, 0.0f, 0.0f);
+	glRotatef(yRot, 0.0f, 1.0f, 0.0f);
+	// Rozpoczęcie rysowania wachlarza trójkątów
+	glBegin(GL_TRIANGLE_FAN);
+
+	// Czubek stoka jest wspólnym wierzchołkiem wszystkich trójkątów z wachlarza
+	// wysuniętym do góry w osi z. W ten sposób zamiast koła powstanie stożek.
+	glVertex3f(0.0f, 0.0f, 75.0f);
+	// Wykonujemy obrót w około i oznaczamy w równych odstępach wierzchołki
+	// tworzące wachlarz trójkątów.
+	for (angle = 0.0f; angle < (2.0f*GL_PI); angle += (GL_PI / 7.0f))
+	{
+		// Wyliczenie współrzędnych x i y kolejnego wierzchołka
+		x = 50.0f*sin(angle);
+		y = 50.0f*cos(angle);
+		// Wybieranie koloru - zielony lub czerwony
+		if ((iPivot % 2) == 0)
+			glColor3f(0.0f, 1.0f, 0.0f);
+		else
+			glColor3f(1.0f, 0.0f, 0.0f);
+		// Inkrementacja zmiennej okrelającej rodzaj koloru
+		iPivot++;
+		// Definiowanie kolejnego wierzchołka w wachlarzu trójkątów
+		glVertex2f(x, y);
+	}
+	// Zakoczenie rysowania trójkątów stożka
+	glEnd();
+	// Rozpoczęcie rysowania kolejnego wachlarza trójkątów
+	// zakrywającego podstaw stoka
+	glBegin(GL_TRIANGLE_FAN);
+	// środek wachlarza znajduje się na początku układu współrzędnych
+	glVertex2f(0.0f, 0.0f);
+	for (angle = 0.0f; angle < (3.0f*GL_PI); angle += (GL_PI / 7.0f))
+	{
+		// Wyliczenie współrzędnych x i y kolejnego wierzchołka
+		x = 50.0f*sin(angle);
+		y = 50.0f*cos(angle);
+		// Wybieranie koloru - zielony lub czerwony
+		if ((iPivot % 2) == 0)
+			glColor3f(0.0f, 1.0f, 0.0f);
+		else
+			glColor3f(1.0f, 0.0f, 0.0f);
+		// Inkrementacja zmiennej okrelającej rodzaj koloru
+		iPivot++;
+		// Definiowanie kolejnego wierzchołka w wachlarzu trójkątów
+		glVertex2f(x, y);
+	}
+	// Zakoczenie rysowania trójkątów podstawy stoka
+	glEnd();
+	// Odtworzenie macierzy przekształceń
+	glPopMatrix();
+	// Wysłanie poleceń do wykonania
+	glutSwapBuffers();
+}
+
+void SpecialKeys(int key, int x, int y)
+{
+	if (key == GLUT_KEY_UP)
+		xRot -= 5.0f;
+	if (key == GLUT_KEY_DOWN)
+		xRot += 5.0f;
+	if (key == GLUT_KEY_LEFT)
+		yRot -= 5.0f;
+	if (key == GLUT_KEY_RIGHT)
+		yRot += 5.0f;
+	if (key > 356.0f)
+		xRot = 0.0f;
+	if (key < -1.0f)
+		xRot = 355.0f;
+	if (key > 356.0f)
+		yRot = 0.0f;
+	if (key < -1.0f)
+		yRot = 355.0f;
+	// Odświeżenie zawartości okna
+	glutPostRedisplay();
+}
+void ChangeSize(int w, int h)
+{
+	GLfloat nRange = 100.0f;
+	// Zabezpieczenie przed dzieleniem przez zero
+	if (h == 0)
+		h = 1;
+	// Ustalenie wymiarów widoku na zgodnych z wymiarami okna
+	glViewport(0, 0, w, h);
+	// Ponowne ustawienie stosu macierzy rzutowania
+	glMatrixMode(GL_PROJECTION);
+	glLoadIdentity();
+	// Utworzenie przestrzeni ograniczającej (lewo, prawo, dół, góra, blisko, daleko)
+	if (w <= h)
+		glOrtho(-nRange, nRange, -nRange*h / w, nRange*h / w, -nRange, nRange);
+	else
+		glOrtho(-nRange*w / h, nRange*w / h, -nRange, nRange, -nRange, nRange);
+	// Ponowne ustawienie stosu macierzy rzutowania
+	glMatrixMode(GL_MODELVIEW);
+	glLoadIdentity();
+}
+
+void Menu(int value)
+{
+	switch (value)
+	{
+		//
+
+	case GL_SMOOTH:
+		iSmooth = !iSmooth;
+		glutSetWindowTitle("Stozek - GL_smoothtest");
+		RenderScene();
+		break;
+
+	case GL_CULL_FACE:
+		/*iCull =! iCull;*/
+		glutSetWindowTitle("Stozek - GL_CULL_FACE");
+		RenderScene();
+		break;
+
+		//
+	case GL_DEPTH_TEST:
+		iDepth = !iDepth;
+		glutSetWindowTitle("Stozek - GL_DEPTH_TEST");
+		RenderScene();
+		break;
+
+		//
+	case GL_LINE:
+		iOutline = true;
+		glutSetWindowTitle("Stozek - GL_LINE");
+		RenderScene();
+		break;
+
+		//
+	case GL_FILL:
+		iOutline = false;
+		glutSetWindowTitle("Stozek - GL_LINE");
+		RenderScene();
+		break;
+
+
+
+		// wyjęcie
+	case EXIT:
+		exit(0);
+	}
+}
+
+int main(int argc, char* argv[])
+{
+	glutInit(&argc, argv);
+	glutInitDisplayMode(GLUT_DOUBLE | GLUT_RGB | GLUT_DEPTH);
+	glutCreateWindow("Triangles Constructing Fan Example");
+	glutReshapeFunc(ChangeSize);
+	glutSpecialFunc(SpecialKeys);
+	glutDisplayFunc(RenderScene);
+	SetupRC();
+	// utworzenie podmenu - Prymitywy
+	//int MenuPrimitive = glutCreateMenu(Menu);
+	//glutAddMenuEntry("GL_CULL_FACE", GL_CULL_FACE);
+	//glutAddMenuEntry("GL_DEPTH_TEST", GL_DEPTH_TEST);
+	//glutAddMenuEntry("Polygon mode LINE", GL_LINE);
+	//glutAddMenuEntry("Polygon mode FILL", GL_FILL);
+	//glutAddMenuEntry("smoth", GL_SMOOTH);
+	//glutAddMenuEntry("Wyjecie", EXIT);
+
+	//// określenie przycisku myszki obsługującego menu podręczne
+	//glutAttachMenu(GLUT_RIGHT_BUTTON);
+
+	glutMainLoop();
+	return 0;
+}
